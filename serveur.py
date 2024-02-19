cd25561615a20b5b6f8ef6ac339cb666c859b7dd
unknown <aymane.daoudou@icloud.com>
2024-02-19 12:07:04 +0100

    Fix : Définition d'une fonction permettant de générer la carte du jeu

--- serveur.py
+++ serveur.py
@@ -1,2 +1,24 @@
-if __name__ == "main":
-    
+import sys
+
+def generate_card(tuple1 : list) -> list:
+    tableau = []
+    print(type(tuple1[0]) == int)
+    if tuple1[0] != tuple1[1]:
+        print("Les deux valeurs inscrites dans le tuple doivent être égales")
+        return
+    if type(tuple1[0]) is not int:
+        print("Les données doivent être des nombres entiers")
+        return
+    if tuple1[0] < 10:
+        print("Les deux nombres doivent être supérieurs à 10")
+        return
+    for i in range(tuple1[0]):
+        t = []
+        for j in range(tuple1[1]):
+            t.append("*")
+        tableau.append(t)
+    return tableau
+
+if __name__ == '__main__':
+   tmp = generate_card([10,10])
+   print(tmp)
